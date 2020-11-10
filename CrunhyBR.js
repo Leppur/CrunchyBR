dafd63eae6db165b1e6334cc7aafefe304fa84d2
Leppur <brynjar04@gmail.com>
2020-11-10 21:02:57 +0000

    added github link in js file

--- CrunhyBR.js
+++ CrunhyBR.js
@@ -1,13 +1,16 @@
 // ==UserScript==
-// @name         Crunhyroll banner remover
+// @name         CrunhyBR, Crunchyroll banner remover
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Removes banners and notifications from crunchyroll
-// @author       Brynjar
+// @author       Leppur
 // @match        https://www.crunchyroll.com/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
+//
+// github        https://github.com/Leppur/CrunchyBR
 
 document.getElementById("marketing_banner").remove();
 document.getElementById("message_box").remove();
+
